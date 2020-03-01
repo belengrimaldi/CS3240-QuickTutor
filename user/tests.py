055c1be0da5b2d9ad1bc4a1fdb1420728e8c3929
Dan Rayder <dsr8cm@virginia.edu>
2020-03-01 17:20:23 -0500

    first test

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.db import models
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        Profile.user.create(user = User,
+                            bio = "Hello, tutor me",
+                            location = "America",
+                            birth_date = "04/09/1999",
+                            year = 2021,
+                            classes_taken = "Art",
+                            help_needed = "CS 3240",
+                            image = "default.jpg"
+        )
+
+    def bioTest(self):
+        testU = Profile.user.get(bio="Hello, tutor me")
+        self.assertEqual(testU.bio, "Hello, tutor me")

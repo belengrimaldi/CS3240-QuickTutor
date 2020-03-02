d922554accbedcda2b31fd2a18a3326fb44cf315
es7za <es7za@virginia.edu>
2020-03-01 23:07:53 -0500

    adding all the tests

--- user/tests.py
+++ user/tests.py
@@ -19,3 +19,29 @@ class UserTestCase(TestCase):
     def bioTest(self):
         testU = Profile.user.get(bio="Hello, tutor me")
         self.assertEqual(testU.bio, "Hello, tutor me")
+
+    def locationTest(self):
+        testU = Profile.user.get(location="America")
+        self.assertEqual(testU.location, "America")
+
+    def birth_dateTest(self):
+        testU = Profile.user.get(birth_date="04/09/1999")
+        self.assertEqual(testU.birth_date, "04/09/1999")
+
+    def yearTest(self):
+        testU = Profile.user.get(year=2021)
+        self.assertEqual(testU.year, 2021)
+
+    def classes_TakenTest(self):
+        testU = Profile.user.get(classes_taken="Art")
+        self.assertEqual(testU.classes_taken, "Art")
+
+    def help_neededTest(self):
+        testU = Profile.user.get(help_needed="CS 3240")
+        self.assertEqual(testU.help_needed,"CS 3240")
+
+    def help_neededTest(self):
+        testU = Profile.user.get(image="default.jpg")
+        self.assertEqual(testU.image,"default.jpg")
+    
+    

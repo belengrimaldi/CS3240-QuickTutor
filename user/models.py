ee54dc0ef7e05ab2198bf66f0171cea343509da7
Belen Gomez Grimaldi <mbg3qz@virginia.edu>
2020-03-02 15:12:40 -0500

    styling update form

--- user/models.py
+++ user/models.py
@@ -10,7 +10,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
     year = models.PositiveSmallIntegerField(blank=True, null=True)
     classes_taken = models.TextField(max_length=400, blank=True)
     help_needed = models.TextField(max_length=300, blank=True)

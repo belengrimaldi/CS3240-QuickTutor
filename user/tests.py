0750c50d55269f8075065aeaea42cef1fef48849
Belen Gomez Grimaldi <mbg3qz@virginia.edu>
2020-03-28 16:34:00 -0500

    active tutor test try

--- user/tests.py
+++ user/tests.py
@@ -1,10 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.db import models
+
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Message
 
 class UserTestCase(TestCase):
     def setUp(self):
+
         User.objects.create(email='example@example.com', first_name = 'Emma', last_name = 'Studtmann')
 
     def test_UserExists_Test(self):
@@ -16,4 +18,38 @@ class UserTestCase(TestCase):
         #profile = Profile.objects.filter(bio='Tutor me') This will return a list of all of the profiles which have this bio
         #profile = Profile.objects.filter(bio='Tutor me').filter(location = 'Roanoke') This will return a list of all of the profiles which have this bio
         self.assertNotEquals(my_profile, None)
+
+
+# class ActiveTutorTestCase(TestCase):
+#     def setUp(self):
+#         User.objects.create(email='example@example.com', first_name = 'Emma', last_name = 'Studtmann')
+    
+#     def test_activeTutorSetting(self):
+#         my_user = User.objects.get(email='example@example.com')
+#         my_profile = Profile.objects.get(user=my_user) #This will just return the one user's profile
+#         my_profile.active_tutor = True
+
+
+class MessageTestCase(TestCase):
+    def setUp(self):
+
+        User.objects.create(email='row@row.com', first_name = 'Rowan', last_name = 'Dakota', username='row')
+        User.objects.create(email='foo@foo.com', first_name = 'Foo', last_name = 'Bar', username='foo')
+
+    def test_Message(self):
+
+        foo = User.objects.get(email='foo@foo.com')
+        row = User.objects.get(email='row@row.com')
+
+
+        msg = Message(
+                sender = foo,
+                receiver = row,
+                msg_content = "What's up my hommie g dog?",
+        )
+
+        msg.save()
+        re_msg = Message.objects.get(msg_content="What's up my hommie g dog?")
+        self.assertEqual(re_msg.sender, foo)
+        self.assertEqual(re_msg.receiver, row)
 

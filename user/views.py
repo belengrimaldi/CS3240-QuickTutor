6fa36d4160b7e7e4288c1638ea97621474ad5b69
FrostlyLeaves <59891878+FrostlyLeaves@users.noreply.github.com>
2020-03-29 16:37:05 -0400

    Correspondence list in right order

--- user/views.py
+++ user/views.py
@@ -73,32 +73,14 @@ def CorLog(request, pal_username):
         coris.append(i)
     for i in sent:
         coris.append(i)
-    coris.sort(key=(lambda x: x.created_at))
+    coris.sort(key=(lambda x: x.created_at), reverse=True)
 
     context = {
         'coris': coris,
         'pal' : pen_pal
     }
     return render(request, 'log.html', context)
-"""
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-"""
+
 @login_required
 def SeeProfile(request):
     return render(request, 'profile.html')

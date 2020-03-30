602e2cdb74eac2b44b1a33ff43c19f73693a9aa6
Dan Rayder <dsr8cm@virginia.edu>
2020-03-30 15:25:59 -0400

    simple 404

--- static/views.py
+++ static/views.py
@@ -6,3 +6,8 @@ from django.template import loader
 
 def about(request):
     return render(request, 'static/about.html')
+
+def handler404(request, exception, template_name="404.html"):
+    response = render(request, template_name)
+    response.status_code = 404
+    return response

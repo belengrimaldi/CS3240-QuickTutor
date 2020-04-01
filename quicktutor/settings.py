88061b3717c55aa96b3f27aa7635eb8fbe5eeca9
es7za <es7za@virginia.edu>
2020-03-31 20:37:39 -0400

    TA session

--- quicktutor/settings.py
+++ quicktutor/settings.py
@@ -173,9 +173,7 @@ MEDIA_URL = '/media/'
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='354861153936-10lli7qhi9h8o60vlhu6rsp8rdqa8cpl.apps.googleusercontent.com'  #Paste CLient Key
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '-niiEgaceqgsoQtoyIF_t2WD' #Paste Secret Key
 
-try:
-    # Configure Django App for Heroku.
+import os
+if '/app' in os.environ['HOME']:
     import django_heroku
-    django_heroku.settings(locals())
-except ImportError:
-    found = False
+    django_heroku.settings(locals()) 

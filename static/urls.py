292e605edb6032992622cff8448a3100711aa7b6
Belen Gomez Grimaldi <mbg3qz@virginia.edu>
2020-04-02 17:12:48 -0500

    url fix

--- static/urls.py
+++ static/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('FAQ/', views.FAQ, name='FAQ'),
+    path('about', views.about, name='about'),
+    path('FAQ', views.FAQ, name='FAQ'),
 ]

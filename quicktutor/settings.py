16d164b911fe41cdb8a596f5d3d6086d81359d71
Rowan <rad7eu@virginia.edu>
2020-04-05 21:07:35 -0400

    switched to postgres?

--- quicktutor/settings.py
+++ quicktutor/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-if '/app' in os.environ['HOME']:
-    django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,13 +83,25 @@ WSGI_APPLICATION = 'quicktutor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'team_falcon',
+        'USER': 'postgres',
+        'PASSWORD': 'Htsrtna96!',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators

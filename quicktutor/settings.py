816c8357847662fff90f2e2e6ca986a54700c360
Dan Rayder <56370744+dsr8cm@users.noreply.github.com>
2020-04-12 22:58:30 -0400

    Turning debug back on for now

--- quicktutor/settings.py
+++ quicktutor/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v%)(vi1k4xr@&&t8hzh9h%^sau7!m*c_59vei2p(r%vfn0^@yv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #TEMPLATE_DEBUG = True
 
